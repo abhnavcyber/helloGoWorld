418ae6d9a0cd59b5553c695841d33a6e7567f2fe
Abhinav <169052972+abhnavcyber@users.noreply.github.com>
2024-12-27 13:17:18 +0530

    Update hello-openshift-for-developers.go

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -3,13 +3,13 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	)
+)
 	
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os. Getenv ("RESPONSE")
 	if len (response) == 0 {
-	response = "Hello Abhinav!"
+		response = "Hello Abhinav!"
 	｝
 	fmt. Fprintln(w, response)
 	fmt. Println("Servicing an impatient beginner's request.")
-	}
+}
